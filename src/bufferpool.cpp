52d5edfc672d22cc01496e250c972c991eaa2904
Ryan Williams <ryan@c.oloss.us>
2019-07-29 10:40:23 -0500

    Switched buffer pool to memfd_create

--- src/bufferpool.cpp
+++ src/bufferpool.cpp
@@ -3,50 +3,28 @@
 #include "rwte/wayland.h"
 
 #include <cairo/cairo.h>
-#include <fcntl.h>
 
 #define LOGGER() (logging::get("bufferpool"))
 
 namespace wlwin {
 
-static void randname(char* buf)
-{
-    struct timespec ts;
-    clock_gettime(CLOCK_REALTIME, &ts);
-    long r = ts.tv_nsec;
-    for (int i = 0; i < 6; ++i) {
-        buf[i] = 'A' + (r & 15) + (r & 16) * 2;
-        r >>= 5;
-    }
-}
+// we should really only need one buffer, right?
+const int NumBuffers = 2;
 
 static int create_shm_file(off_t size)
 {
-    char name[] = "/rwte-XXXXXX";
-    int retries = 100;
-
-    do {
-        randname(name + strlen(name) - 6);
-
-        --retries;
-        // shm_open guarantees that O_CLOEXEC is set. if it works,
-        // we can unlink it immediately; nobody cares about its name.
-        int fd = shm_open(name, O_RDWR | O_CREAT | O_EXCL, 0600);
-        if (fd >= 0) {
-            shm_unlink(name);
-            if (ftruncate(fd, size) < 0) {
-                close(fd);
-                return -1;
-            }
+    // nobody cares what it's called, we're going to
+    // close it after we make the exchange
+    int fd = memfd_create("", MFD_CLOEXEC);
+    if (fd >= 0) {
+        if (ftruncate(fd, size) >= 0)
             return fd;
-        }
-    } while (retries > 0 && errno == EEXIST);
+
+        close(fd);
+    }
 
     return -1;
 }
-
-// we should really only need one buffer, right?
-const int NumBuffers = 2;
 
 Buffer::Buffer(BufferPool* pool, wl_buffer* buffer, unsigned char* data,
         int width, int height, int stride) :
@@ -77,7 +55,7 @@ bool BufferPool::create_buffers(int width, int height)
     for (int i = 0; i < NumBuffers; i++) {
         int fd = create_shm_file(size);
         if (fd < 0) {
-            LOGGER()->fatal("creating a buffer file failed for {}: {}\n",
+            LOGGER()->fatal("creating a buffer fd failed for {}: {}\n",
                     size, strerror(errno));
             return false; // won't return
         }
