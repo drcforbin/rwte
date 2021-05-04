51563d9e549ac1aa38a2107529b90085c6c0f577
drcforbin <ryan@c.oloss.us>
2021-05-04 12:41:16 -0500

    moved utf8 to librw

--- bench/main.cpp
+++ bench/main.cpp
@@ -2,18 +2,19 @@
 #include "nanobench.h"
 
 #include "rw/logging.h"
+#include "rw/utf8.h"
+
+#define LOGGER() (rw::logging::get("rwte-bench"))
 
 // todo: better way than extern.
 
 void bench_string_cmp(ankerl::nanobench::Config& cfg);
-void bench_utf8_encoding(ankerl::nanobench::Config& cfg);
-void bench_utf8_decoding(ankerl::nanobench::Config& cfg);
 
 int main()
 {
+    rw::utf8::set_locale();
+
     auto cfg = ankerl::nanobench::Config();
 
     bench_string_cmp(cfg);
-    bench_utf8_encoding(cfg);
-    bench_utf8_decoding(cfg);
 }
--- test/main.cpp
+++ test/main.cpp
@@ -2,7 +2,14 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest.h"
 
+#include "rw/logging.h"
+#include "rw/utf8.h"
+
+#define LOGGER() (rw::logging::get("rwte-bench"))
+
 int main(int argc, char** argv)
 {
+    rw::utf8::set_locale();
+
     return doctest::Context(argc, argv).run();
 }
